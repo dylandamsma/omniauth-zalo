f173be94573f30de786a5fb5f14f30c84860ab37
Dylan Damsma <dylandamsma@gmail.com>
2023-12-27 13:50:15 +0800

    Update zalo.rb

--- lib/omniauth/strategies/zalo.rb
+++ lib/omniauth/strategies/zalo.rb
@@ -11,14 +11,12 @@ module OmniAuth
       option :client_options, {
         site: 'https://oauth.zaloapp.com',
         authorize_url: '/v4/permission',
-        token_url: '/v4/access_token',
-        token_method: :post,
-        grant_type: 'authorization_code',
-        code_challenge: 'is5SvnFPQzBNP-nb-poEaFlsvK1a6S3NpVCz0vcHh0w',
-        code_verifier: 'h57bycdwryntewreomnbSyDrAG4kX7BeqS7g-luzvBE'
+        token_url: '/v4/access_token'
       }
 
-      option :provider_ignores_state, true
+      option :pkce, true
+
+      # option :provider_ignores_state, true
 
       uid { raw_info['id'] }
 
@@ -29,27 +27,27 @@ module OmniAuth
         }
       end
 
-      def authorize_params
-        super.merge(app_id: self.options.client_id, code_challenge: options.client_options.code_challenge)
-      end
+      # def authorize_params
+        # super.merge(app_id: self.options.client_id, code_challenge: options.client_options.code_challenge)
+      # end
 
-      def build_access_token
-        oauth2_access_token
-      end
+      #def build_access_token
+      #  oauth2_access_token
+      #end
 
-      def raw_info
-        get_user_info
-      end
+      #def raw_info
+      #  get_user_info
+      #end
 
-      alias :old_callback_url :callback_url
+     # alias :old_callback_url :callback_url
 
-      def callback_url
-        if request.params['callback_url']
-          request.params['callback_url']
-        else
-          old_callback_url
-        end
-      end
+      # def callback_url
+      #  if request.params['callback_url']
+      #    request.params['callback_url']
+      #  else
+      #    old_callback_url
+      #  end
+      # end
     end
   end
 end
